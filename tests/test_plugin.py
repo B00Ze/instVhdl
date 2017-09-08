03ab3d443a4019de0cb938f33ba4063b272ff83f
BooZe <buzz.sh@tut.by>
2017-09-08 18:36:27 +0300

    Add fixtures instead of direct objects creation

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import pytest
+import string
+import random
 
 from plugin import instVHDL
 
@@ -20,44 +22,52 @@ def test_port_creation():
     assert p.getType() == p_type
 
 
-def test_generic_port_creation():
-    p_name = "p_name"
-    p_type = "p_type"
-    p_default = "p_default"
-    p = instVHDL.genericPort(p_name, p_type, p_default)
-    assert p.getName() == p_name
-    assert p.getType() == p_type
-    assert p.getDefault() == p_default
+DEFAULT_P_NAME    = "p_name"
+DEFAULT_P_TYPE    = "p_type"
+DEFAULT_P_DEFAULT = "p_default"
 
-def test_generic_port_vhdl_creation():
-    p_name = "p_name"
-    p_type = "p_type"
-    p_default = "p_default"
+@pytest.fixture()
+def generic_port():
+    p_name    = DEFAULT_P_NAME
+    p_type    = DEFAULT_P_TYPE
+    p_default = DEFAULT_P_DEFAULT
+    p = instVHDL.genericPort(p_name, p_type, p_default)
+    return p
+
+def test_generic_port_creation(generic_port):
+    p = generic_port
+    assert p.getName() == DEFAULT_P_NAME
+    assert p.getType() == DEFAULT_P_TYPE
+    assert p.getDefault() == DEFAULT_P_DEFAULT
+
+@pytest.fixture()
+def generic_port_vhdl():
+    p_name    = DEFAULT_P_NAME
+    p_type    = DEFAULT_P_TYPE
+    p_default = DEFAULT_P_DEFAULT
     p = instVHDL.genericPortVHDL(p_name, p_type, p_default)
+    return p
+
+def test_generic_port_vhdl_creation(generic_port_vhdl):
+    p = generic_port_vhdl
     assert len(p.getStrList()) != 0
-    assert p_name in TEST_JOIN_SIGN.join(p.getStrList())
-    assert p_type in TEST_JOIN_SIGN.join(p.getStrList())
-    
-def test_generic_port_vhdl_alignment():
-    p_name = "p_name"
-    p_type = "p_type"
-    p_default = "p_default"
-    p = instVHDL.genericPortVHDL(p_name, p_type, p_default)
-    alignments = (0, 5, 33, len(p_name), len(p_name)-1)
+    assert DEFAULT_P_NAME in TEST_JOIN_SIGN.join(p.getStrList())
+    assert DEFAULT_P_TYPE in TEST_JOIN_SIGN.join(p.getStrList())
+
+def test_generic_port_vhdl_alignment(generic_port_vhdl):
+    p = generic_port_vhdl
+    alignments = (0, 5, 33, len(DEFAULT_P_NAME), len(DEFAULT_P_NAME)-1)
     for alignment in alignments:
-        colon_position = get_alignment(p_name, alignment)
+        colon_position = get_alignment(DEFAULT_P_NAME, alignment)
         aligned = TEST_JOIN_SIGN.join(p.getStrAligned(alignment))
         assert aligned[colon_position] == ':'
 
-def test_generic_port_vhdl_default():
-    p_name = "p_name"
-    p_type = "p_type"
-    p_default = "p_default"
+def test_generic_port_vhdl_default(generic_port_vhdl):
+    p = generic_port_vhdl
     alignment = 0
-    p = instVHDL.genericPortVHDL(p_name, p_type, p_default)
 
     test_str = TEST_JOIN_SIGN.join(p.getStrAligned(alignment))
-    assert p_default in test_str
+    assert DEFAULT_P_DEFAULT in test_str
 
     default = "tmp"
     p.setDefault(default)
@@ -102,13 +112,20 @@ def tests_port_inout_vhdl_aligned():
             assert inout_position > 0
             assert aligned[inout_position+inout_colon_offset - 1] == ' '
 
-def tests_component_creation():
-    c_name = "c_name"
-
+DEFAULT_C_NAME = "c_name"
+@pytest.fixture()
+def dummy_component():
+    c_name = DEFAULT_C_NAME
     c = instVHDL.component(c_name)
+    return c
+
+def tests_component_creation(dummy_component):
+    c_name = DEFAULT_C_NAME
+
+    c = dummy_component
 
     assert c.getName() == c_name
-    assert len(c.getLib()) > 0 
+    assert len(c.getLib()) > 0
     assert len(c.getGeneric()) == 0
 
     c_name = "new_name"
@@ -118,3 +135,28 @@ def tests_component_creation():
     c_lib = "c_lib"
     c.setLib(c_lib)
     assert c.getLib() == c_lib
+
+DEFAULT_MAX_NAME_LENGTH = 30
+
+def random_name():
+    valid_sumbols = string.ascii_letters + string.digits + "_"
+    name_length = random.randint(1, DEFAULT_MAX_NAME_LENGTH)
+    return ''.join(random.choice(valid_sumbols) for x in range(name_length))
+
+@pytest.fixture()
+def generic_list():
+    MAX_LIST_LENGTH = 20
+    out = []
+    for _ in range(random.randint(3, MAX_LIST_LENGTH)):
+        p_name    = random_name()
+        p_type    = random_name()
+        p_default = random_name()
+        p = instVHDL.genericPort(p_name, p_type, p_default)
+        out.append(p)
+    return out
+
+def tests_component_generic_add(generic_list):
+    pass
+#    for generic in generic_list:
+#        assert generic.getName() in
+
